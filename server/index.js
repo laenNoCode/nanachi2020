5c441b922c320c30e806c43e2db9d435baf84282
laenNoCode <youen.froger@etu.emse.fr>
2020-12-03 22:12:33 +0100

    add of database (partial)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const databaseCreator = require("./database/databaseCreator")
+
+databaseCreator.createDB("./database/data.db").then(()=>{
+  console.log("database created")
+}).catch()
+
+
 var app = express()
 var login = require("./middlewares/login")
 
@@ -7,8 +14,11 @@ app.use(bodyParser.urlencoded({
     extended: false
   }));
 app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
 app.use(login.login)
+app.use(login.create)
+
 app.use(express.static("./public"))
-
-
+console.log("server started")
 app.listen("8080")
--- server/middlewares/login.js
+++ server/middlewares/login.js
@@ -1,4 +1,44 @@
+base64url="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
 exports.login = function(req,res,next){
-    res.send(Object.keys(req.body))
+    
+    bodyKeys = Object.keys(req.body)
+    console.log({bodyKeys,in:bodyKeys.includes("username")})
+    if (bodyKeys.includes("username") && bodyKeys.includes("password")){
+        console.log("user can log in")
+        console.log(req.body)
+    }
+    next()
+}
+
+exports.create = function(req,res,next){
+    const sqlite = require("sqlite3").verbose()
+    const crypto = require("crypto")
+
+    bodyKeys = Object.keys(req.body)
+    console.log({bodyKeys,in:bodyKeys.includes("password")})
+    if (bodyKeys.includes("username") && bodyKeys.includes("password")){
+        console.log("in")
+        let db = new sqlite.Database("./database/data.db", (err) => {
+            if (err){
+                console.log(err)
+            }
+        })
+        db.each("SELECT * FROM user", (data) => {
+            console.log(data)
+        })
+        salt = ""
+        for (i = 0; i < 16; ++i){
+            j = Math.floor(Math.random() * 64)
+            salt += base64url[j]
+        }
+        console.log("creating hash")
+        const hash = crypto.createHmac('sha256', req.body.password).update(salt).digest('hex');
+        request = "INSERT INTO user(username, salt, hash) VALUES (`" + req.body.username + "`,`" + salt + "`,`" + hash + "`)"
+        console.log(request)
+        db.run(request)
+
+        db.close()
+
+    }
     next()
 }
