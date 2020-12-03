d2b73a31413bee0e2535d514d2683171dbc5c482
laenNoCode <youen.froger@etu.emse.fr>
2020-12-04 00:56:48 +0100

    change, bug of xhr never comming back

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  i18n,
-  render: function (h) { return h(App) }
+  render: function (h) { return h(App) },
 }).$mount('#app')
